4d358c91e785e35770c13732e38a4565b7705143
Jurgen Kluft <jurgen@virtuos.com.cn>
2011-04-11 10:45:38 +0800

    *: unittest now has a standardized main entry

--- source/test/cpp/test_main.cpp
+++ source/test/cpp/test_main.cpp
@@ -19,11 +19,6 @@ public:
 		mAllocator = allocator;
 	}
 
-	void	Release()
-	{
-		mAllocator->release();
-	}
-
 	void*	Allocate(int size)
 	{
 		++mNumAllocations;
@@ -34,40 +29,24 @@ public:
 		--mNumAllocations;
 		mAllocator->deallocate(ptr);
 	}
-};
-
-
-class UnitTestObserver : public UnitTest::Observer
-{
-public:
-	void	BeginFixture(const char* filename, const char* suite_name, const char* fixture_name)
+	void	Release()
 	{
-	}
-	void	EndFixture()
-	{
+		mAllocator->release();
 	}
 };
 
-
-int main(int argc, char** argv)
+bool gRunUnitTest(UnitTest::TestReporter& reporter)
 {
 	UnitTestAllocator unittestAllocator( xcore::gCreateSystemAllocator() );
-	UnitTestObserver unittestObserver;
 	UnitTest::SetAllocator(&unittestAllocator);
-	UnitTest::SetObserver(&unittestObserver);
-
-	UnitTest::TestReporterStdout stdout_reporter;
-	UnitTest::TestReporter& reporter = stdout_reporter;
 
 	int r = UNITTEST_SUITE_RUN(reporter, xHashUnitTest);
-
 	if (unittestAllocator.mNumAllocations!=0)
 	{
-		reporter.reportFailure(__FILE__, __LINE__, __FUNCTION__, "memory leaks detected!");
+		reporter.reportFailure(__FILE__, __LINE__, "xunittest", "memory leaks detected!");
 		r = -1;
 	}
 
 	unittestAllocator.Release();
-	return r;
-
+	return r==0;
 }
