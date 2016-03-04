ed2e1bc695ac5b208b20365695973d91ceb92a0b
Jurgen <jurgen@virtuos.com.cn>
2016-03-04 18:28:58 +0800

    setting up default cpp project has moved to denv

--- package/package.go
+++ package/package.go
@@ -2,13 +2,12 @@ package xhash
 
 import (
 	"github.com/jurgen-kluft/xbase/package"
-	"github.com/jurgen-kluft/xcode"
 	"github.com/jurgen-kluft/xcode/denv"
 )
 
 // GetProject returns the Project information of 'xhash'
 func GetProject() *denv.Project {
-	xcode.SetupDefaultCppProject("xhash", "github.com\\jurgen-kluft")
+	denv.SetupDefaultCppProject("xhash", "github.com\\jurgen-kluft")
 	project.Dependencies = append(project.Dependencies, xbase.GetProject())
 
 	return project
