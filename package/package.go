f81c5bb85b18a2f2fa7f03b1dc1784135e2472a6
Jurgen <jurgen@virtuos.com.cn>
2016-09-27 11:16:55 +0800

    *: should not import xhash package

--- package/package.go
+++ package/package.go
@@ -4,7 +4,6 @@ import (
 	"github.com/jurgen-kluft/xbase/package"
 	"github.com/jurgen-kluft/xcode/denv"
 	"github.com/jurgen-kluft/xentry/package"
-	"github.com/jurgen-kluft/xhash/package"
 	"github.com/jurgen-kluft/xunittest/package"
 )
 
