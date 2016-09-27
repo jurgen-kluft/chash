706f96195444ffb4cd16a378f6fd929351330abd
Jurgen <jurgen@virtuos.com.cn>
2016-09-27 11:15:57 +0800

    *: .gitignore, do not ignore .go files. *: setup package for xhash

--- package/package.go
+++ package/package.go
@@ -1,19 +1,37 @@
-package xhash
+package xcore
 
 import (
+	"github.com/jurgen-kluft/xbase/package"
 	"github.com/jurgen-kluft/xcode/denv"
+	"github.com/jurgen-kluft/xentry/package"
+	"github.com/jurgen-kluft/xhash/package"
+	"github.com/jurgen-kluft/xunittest/package"
 )
 
-// GetPackage returns the package object of 'xhash'
+// GetPackage returns the package object of 'xcore'
 func GetPackage() *denv.Package {
+	// Dependencies
+	xunittestpkg := xunittest.GetPackage()
+	xentrypkg := xentry.GetPackage()
+	xbasepkg := xbase.GetPackage()
+
+	// The main (xhash) package
+	mainpkg := denv.NewPackage("xcore")
+	mainpkg.AddPackage(xunittestpkg)
+	mainpkg.AddPackage(xentrypkg)
+	mainpkg.AddPackage(xbasepkg)
 
 	// 'xhash' library
 	mainlib := denv.SetupDefaultCppLibProject("xhash", "github.com\\jurgen-kluft\\xhash")
+	mainlib.Dependencies = append(mainlib.Dependencies, xbasepkg.GetMainLib())
 
-	// 'xhash' test project
+	// 'xhash' unittest project
 	maintest := denv.SetupDefaultCppTestProject("xhash_test", "github.com\\jurgen-kluft\\xhash")
+	maintest.Dependencies = append(maintest.Dependencies, xunittestpkg.GetMainLib())
+	maintest.Dependencies = append(maintest.Dependencies, xentrypkg.GetMainLib())
+	maintest.Dependencies = append(maintest.Dependencies, xbasepkg.GetMainLib())
+	maintest.Dependencies = append(maintest.Dependencies, mainlib)
 
-	mainpkg := denv.NewPackage("xhash")
 	mainpkg.AddMainLib(mainlib)
 	mainpkg.AddUnittest(maintest)
 	return mainpkg
